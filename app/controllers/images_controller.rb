6135af2d87975ab643edeed36581648ebc8e6e1a
Emmanuel Nosakhare <nonreactiv@gmail.com>
2017-05-06 16:29:06 +0000

    Clean up

---
+++ app/controllers/images_controller.rb
@@ -0,0 +1,15 @@
+class ImagesController < ApplicationController
+  before_action :get_picture
+  def fit
+
+  end
+
+  def max_width
+
+  end
+
+  private
+  def get_picture
+    
+  end
+end
--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,6 @@ require_relative 'boot'
 
 # require 'rails/all'
 require 'action_controller/railtie'
-require 'rails/test_unit/railtie'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
