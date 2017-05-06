d1a0db2ada711316b3c94ff132d8f282f9bdcece
Emmanuel Nosakhare <nonreactiv@gmail.com>
2017-05-06 17:22:31 +0000

    Added homepage message

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,7 +1,14 @@
-class ImagesController < ApplicationController
-  before_action :get_picture
-  def fit
+require 'open-uri'
 
+class ImagesController < ApplicationController
+  before_action :get_picture, except: [:index]
+
+  def index
+    render plain: 'Welcome to the CDN of TheCommunity'
+  end
+
+  def fit
+    send_data @data.read, type: @data.content_type, disposition: 'inline', stream: 'true', buffer_size: '4096' 
   end
 
   def max_width
@@ -10,6 +17,12 @@ class ImagesController < ApplicationController
 
   private
   def get_picture
-    
+    @width = params[:width]
+    @height = params[:height]
+    @uri = params[:image]
+
+    @data = open("https://thecommunity-assets.s3.amazonaws.com/uploads/#{@uri}")
+  rescue # put a specific error class here ideally
+    render plain: 'Image cannot be processed', status: 500
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'images#index'
   get 'fit/:width/:height/:image' => 'images#fit'
   get 'max/:width/:image' => 'images#max_width'
 end
