e49991bc2e3256dcfbf9d2802686ca6ce34dd10d
Emmanuel Nosakhare <nonreactiv@gmail.com>
2020-04-10 11:48:43 +0100

    Upgrade rails

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,5 @@
-class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
-
+class ApplicationController < ActionController::API
   def render_404
-    render plain: 'We can\'t find that', status: 404
+    render plain: "We can't find that", status: 404
   end
 end
