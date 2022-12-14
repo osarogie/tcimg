e8d80c0694a840eae077e82e290d4f8423d8654f
Emmanuel Nosakhare <nonreactiv@gmail.com>
2022-12-14 14:58:23 +0100

    remove rails project

--- app/controllers/application_controller.rb
+++
@@ -1,5 +0,0 @@
-class ApplicationController < ActionController::API
-  def render_404
-    render plain: "We can't find that", status: 404
-  end
-end
--- config/routes.rb
+++
@@ -1,7 +0,0 @@
-Rails.application.routes.draw do
-  root 'images#index'
-  get 'fit/:width/:height/:image' => 'images#fit'
-  get 'max/:width/:image' => 'images#max_width'
-  get ':img_params/:image' => 'images#with_params'
-  get ':image' => 'images#full'
-end
